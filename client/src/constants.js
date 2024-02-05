81bfd78db007e70d4c508a258c7eac8de5773551
Kartik Katiyar <katiyarkartik0@gmail.com>
2024-02-05 11:43:51 +0530

    endpoint updated

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,10 @@
-export const ENDPOINT = "https://recipe-production-4147.up.railway.app"
+const HOSTED_SERVER_ENDPOINT = "https://recipe-13ct.onrender.com"
+
+let ENDPOINT = "";
+if (process.env.NODE_ENV === "production") {
+  ENDPOINT = HOSTED_SERVER_ENDPOINT;
+} else if (process.env.NODE_ENV === "development") {
+  ENDPOINT = "http://localhost:3001";
+}
+
+export default ENDPOINT;
